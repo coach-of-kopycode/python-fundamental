7809bcd3eec9d9d5f59ccad283b6783e7b7a8d84
todikun <todirahmat123@gmail.com>
2022-05-24 16:51:08 +0700

    perulangan for dan while

---
+++ dasar_sintaksis_perulangan_while.py
@@ -0,0 +1,15 @@
+"""
+Program perulangan membaca buku dengan for
+"""
+
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu')
+
+jumlah_buku_yang_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}')
+
+while jumlah_buku_yang_dibaca < jumlah_buku:
+    jumlah_buku_yang_dibaca = jumlah_buku_yang_dibaca + 1
+    print(f'Buku ke {jumlah_buku_yang_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}')
